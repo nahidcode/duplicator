35b2245462cac1b3265556e26aa6a35209d998e4
Bob Riley <rsriley@gmail.com>
2015-10-09 09:43:29 -0700

    Updated look of pro vs free table

--- views/help/gopro.php
+++ views/help/gopro.php
@@ -26,129 +26,121 @@ require_once(DUPLICATOR_PLUGIN_PATH . '/views/inc.header.php');
 	div.dup-gopro-header b {font-size: 35px}
 	a.dup-check-it-btn {box-shadow: 5px 5px 5px 0px #999 !important; font-size: 20px !important; height:50px !important;   padding:10px 40px 0 40px !important;}
 
+	#comparison-table { margin-top: 35px; border-spacing: 0px;  width: 100%}
+	#comparison-table th { color: #E21906;}
+	#comparison-table td, #comparison-table th { font-size: 1.2rem; padding: 20px; }
+	#comparison-table .feature-column { text-align: left; width: 46%}
+	#comparison-table .check-column { text-align: center; width: 27% }
+	#comparison-table tr:nth-child(2n+2) {background-color: #f6f6f6; }
+
 </style>
 
 <script type="text/javascript">var switchTo5x = true;</script>
 <script type="text/javascript" src="https://ws.sharethis.com/button/buttons.js"></script>
 <script type="text/javascript">stLight.options({publisher: "1a44d92e-2a78-42c3-a32e-414f78f9f484"});</script> 
 
-<div class="wrap dup-wrap">
-	
-    <?php duplicator_header(__("Go Pro!", 'wpduplicator')) ?>
+<div class="wrap dup-wrap" >
+
+	<?php duplicator_header(__("Go Pro!", 'wpduplicator')) ?>
     <hr size="1" />
 
-    <div style="width:850px; margin:auto; text-align: center;">
+    <div style="padding: 30px; background-color:white;min-width:640px; max-width:850px; width:90%; margin:auto; text-align: center;">
 
 		<div style="line-height:28px">
-			
+
 			<h1 style="font-size:38px">
 				<img src="<?php echo DUPLICATOR_PLUGIN_URL ?>assets/img/logo.png" style='text-align:top; margin:-8px 0'  />
 				<?php DUP_Util::_e('Duplicator Pro Has Arrived!') ?>
 			</h1>
-			<h3 style="font-size:20px">
+			<h3 style="margin-top:35px; font-size:20px">
 				<?php DUP_Util::_e('The simplicity of Duplicator') ?>
 				<?php DUP_Util::_e('with the power the professional requires.') ?>
 			</h3>
 		</div>
 
-        <!-- FREE-->
-        <div class="dup-compare-area">
-            <div class="dup-support-hlp-hdrs">
-				<?php DUP_Util::_e('Duplicator Free') ?>
-            </div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Backup Files &amp; Database') ?></a>
-				<div class="info"><?php DUP_Util::_e('Compresses all your WordPress files and database into a compressed snapshot archive file.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Directory Filters') ?></a>
-				<div class="info"><?php DUP_Util::_e('Filter out the directories and file extensions you want to include/exclude in your in your archive file.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Database Table Filters') ?></a>
-				<div class="info"><?php DUP_Util::_e('Filter out only the database tables you want to include/exclude in your database creation script.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Migration Wizard') ?></a>
-				<div class="info"><?php DUP_Util::_e('With just two files (archive &amp; installer.php) move your site to a new location.') ?></div>
-			</div>
-			<div class="feature"><i class="fa fa-times fa-lg"></i></div>
-			<div class="feature"><i class="fa fa-times fa-lg"></i></div>
-			<div class="feature"><i class="fa fa-times fa-lg"></i></div>
-            <div class="feature"><i class="fa fa-times fa-lg"></i></div>
-			<div class="feature"><i class="fa fa-times fa-lg"></i></div>
-            <div class="feature"><i class="fa fa-times fa-lg"></i></div>
-			<div class="feature"><i class="fa fa-times fa-lg"></i></div>
-			
-        </div> 
+       	<table id="comparison-table">
+			<tr>
+				<th class="feature-column">
+					Feature
+				</th>
+				<th class="check-column">
+					Duplicator Free
+				</th>
+				<th class="check-column">
+					Duplicator Pro
+				</th>
+			</tr>
+			<tr>
+				<td class="feature-column">Backup Files & Database</td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">Directory Filters</td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">Database Table Filters</td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">Migration Wizard</td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">Scheduled Backups</td>
+				<td class="check-column"></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">Dropbox Storage</td>
+				<td class="check-column"></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">Google Drive Storage</td>
+				<td class="check-column"></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">FTP Storage</td>
+				<td class="check-column"></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>			
+			<tr>
+				<td class="feature-column">Enhanced Package Engine</td>
+				<td class="check-column"></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">File Filters</td>
+				<td class="check-column"></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+			<tr>
+				<td class="feature-column">Customer Support</td>
+				<td class="check-column"></td>
+				<td class="check-column"><i class="fa fa-check"></i></td>
+			</tr>
+		</table>
 
-        <!-- PRO  -->
-        <div class="dup-compare-area">
-            <div class="dup-support-hlp-hdrs">
-				<?php DUP_Util::_e('Duplicator Pro') ?>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Backup Files &amp; Database') ?></a>
-				<div class="info"><?php DUP_Util::_e('Compresses all your WordPress files and database into a compressed snapshot archive file.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Directory Filters') ?></a>
-				<div class="info"><?php DUP_Util::_e('Filter out the directories and file extensions you want to include/exclude in your in your archive file.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Database Table Filters') ?></a>
-				<div class="info"><?php DUP_Util::_e('Filter out only the database tables you want to include/exclude in your database creation script.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Migration Wizard') ?></a>
-				<div class="info"><?php DUP_Util::_e('With just two files (archive &amp; installer.php) move your site to a new location.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Scheduled Backups') ?></a>
-				<div class="info"><?php DUP_Util::_e('Automate the creation of your packages to run at various scheduled intervals.') ?></div>
-			</div>
-            <div class="feature">
-				<img style="width: 22px; vertical-align: bottom;  margin-right: 3px;" src="<?php echo DUPLICATOR_PLUGIN_URL ?>assets/img/dropbox_128px.png" />
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Dropbox Support') ?></a>
-				<div class="info"><?php DUP_Util::_e('Backup up your entire site to Dropbox.') ?></div>
-			</div>
-            <div class="feature">
-				<img style="width: 22px; vertical-align: bottom;  margin-right: 3px;" src="<?php echo DUPLICATOR_PLUGIN_URL ?>assets/img/ftp-64.png" />
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('FTP Support') ?></a>
-				<div class="info"><?php DUP_Util::_e('Backup up your entire site to an FTP server.') ?></div>
-			</div>
-			<div class="feature">
-				<img style="width: 22px; vertical-align: bottom;  margin-right: 3px;" src="<?php echo DUPLICATOR_PLUGIN_URL ?>assets/img/google_drive_64px.png" />
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Google Drive Support') ?></a>
-				<div class="info"><?php DUP_Util::_e('Backup up your entire site to Google Drive.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Customer Support') ?></a>
-				<div class="info"><?php DUP_Util::_e('Server setups can be quite complex, with pro you get prompt help to get your site backed up and moved.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Enhanced Package Engine') ?></a>
-				<div class="info"><?php DUP_Util::_e('Overhauled package engine is faster and processes larger sites without timing-out.') ?></div>
-			</div>
-			<div class="feature">
-				<a href="javascript:void(0)" class="dup-info-click"><?php DUP_Util::_e('Package Templates') ?></a>
-				<div class="info"><?php DUP_Util::_e('Reusable package configurations for flexible backups and custom site generation.') ?></div>
-			</div>
-			
-        </div>
+
         <br style="clear:both" />
         <p style="text-align:center">
             <a href="http://snapcreek.com/duplicator?free-go-pro" target="_blank" class="button button-primary button-large dup-check-it-btn" >
-                <?php DUP_Util::_e('Check It Out!') ?>
+				<?php DUP_Util::_e('Check It Out!') ?>
             </a>
         </p>
     </div>
 </div><br/><br/><br/><br/>
 
 <script type="text/javascript">
-	jQuery(document).ready(function($) {
-		$( "a.dup-info-click" ).click(function() {
-			$(this).parent().find('.info').toggle();
-		});
-	});
+    jQuery(document).ready(function ($) {
+        $("a.dup-info-click").click(function () {
+            $(this).parent().find('.info').toggle();
+        });
+    });
 </script>
