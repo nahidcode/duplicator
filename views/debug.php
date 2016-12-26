ca00907d9c4e6b1af4dbaa879f2f4b8d7eba6592
lifeinthegrid <cory@lifeinthegrid.com>
2016-12-25 22:41:30 -0700

    - New Controller model layout - Improved Scan Validator

--- views/debug.php
+++ views/debug.php
@@ -5,17 +5,13 @@ require_once(DUPLICATOR_PLUGIN_PATH . '/assets/js/javascript.php');
 require_once(DUPLICATOR_PLUGIN_PATH . '/views/inc.header.php');
 ?>
 <style>
-
-
+	div.debug-area form {margin: 10px 0 0 0}
 </style>
-
-
 
 <div class="wrap dup-wrap dup-support-all">
 	
     <?php duplicator_header(__("Debug", 'duplicator')) ?>
     <hr size="1" />
-
 
 	<div class="debug-area">
 		<h2>Controller Debug</h2>
@@ -23,13 +19,17 @@ require_once(DUPLICATOR_PLUGIN_PATH . '/views/inc.header.php');
 		<form action="admin-ajax.php?action=DUP_CTRL_Tools_RunScanValidator" method="post" target="duplicator_debug">
 			<?php wp_nonce_field('DUP_CTRL_Tools_RunScanValidator', 'nonce'); ?>
 			<b>DUP_CTRL_Tools_RunScanValidator:</b> <br/>
-			<input type="submit" class="button button-small" value="Run Test"/>
+			<a href="javascript:void(0)" onclick="jQuery(this).parent('form').submit()">[Run Test]</a>
+		</form>
+		
+		<form action="admin-ajax.php?action=duplicator_package_scan" method="post" target="duplicator_debug">
+			<?php wp_nonce_field('duplicator_package_scan', 'nonce'); ?>
+			<b>duplicator_package_scan:</b> <br/>
+			<a href="javascript:void(0)" onclick="jQuery(this).parent('form').submit()">[Run Test]</a>
 		</form>
 	
 		
 	</div>
-
-
 
 </div>
 
