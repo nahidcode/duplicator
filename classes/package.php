e0632d6f0cca02d704f0778c7c769fa062a2c773
lifeinthegrid <cory@lifeinthegrid.com>
2017-02-17 21:14:33 -0700

    - new dialog framework integration

--- classes/package.php
+++ classes/package.php
@@ -209,7 +209,7 @@ class DUP_Package {
 			if ($results === false) {
 						
 				$wpdb->print_error();
-				DUP_PRO_Log::Error("Duplicator is unable to insert a package record into the database table.", "'{$wpdb->last_error}'");
+				DUP_Log::Error("Duplicator is unable to insert a package record into the database table.", "'{$wpdb->last_error}'");
 			}
 			$this->ID = $wpdb->insert_id;
 		}
