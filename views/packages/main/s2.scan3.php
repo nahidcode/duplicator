487436ab069b58faff1268cb580b5a71981c9d54
lifeinthegrid <cory@lifeinthegrid.com>
2017-06-13 11:12:58 -0700

    - improve scan quick filters for files

--- views/packages/main/s2.scan3.php
+++ views/packages/main/s2.scan3.php
@@ -90,7 +90,7 @@ TOTAL SIZE -->
 						{{#each ARC.FilterInfo.TreeSize as |directory|}}
 							<div class="directory">
 								<i class="fa fa-caret-right fa-lg dup-nav" onclick="Duplicator.Pack.toggleDirPath(this)"></i> &nbsp;
-								<input type="checkbox" name="dir_paths[]" value="{{directory.dir}}" id="lf_dir_{{@index}}" />
+								<input type="checkbox" name="dir_paths[]" value="{{directory.dir}}" id="lf_dir_{{@index}}" onclick="Duplicator.Pack.filesOff(this)" />
 								<label for="lf_dir_{{@index}}" title="{{directory.dir}}">
 									<i class="size">[{{directory.size}}]</i> /{{directory.sdir}}/
 								</label> <br/>
@@ -375,6 +375,15 @@ jQuery(document).ready(function($)
 	Handlebars.registerHelper('dirSize', function(path) {
 		return  (path.length > 70) ? path.slice(0, 70) + '...' : path;
 	});
+
+	//Opens a dialog to show scan details
+	Duplicator.Pack.filesOff = function (dir)
+	{
+		var $checks = $(dir).parent('div.directory').find('div.files input[type="checkbox"]');
+		$(dir).is(':checked')
+			? $.each($checks, function() {$(this).attr({disabled : true, checked : false});})
+			: $.each($checks, function() {$(this).removeAttr('disabled checked');});
+	}
 
 	//Opens a dialog to show scan details
 	Duplicator.Pack.showDetails = function ()
