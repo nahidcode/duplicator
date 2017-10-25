99002e2ccaacf27104495694290eee8eec4f9a76
saharakks <saharakks@yandex.com>
2017-10-26 01:39:43 +0500

    Information old link missing fix

--- views/tools/diagnostics/information.php
+++ views/tools/diagnostics/information.php
@@ -1,4 +1,4 @@
-<form id="dup-settings-form" action="<?php echo admin_url( 'admin.php?page=duplicator-tools&tab=diagnostics&section=diagnostic' ); ?>" method="post">
+<form id="dup-settings-form" action="<?php echo admin_url( 'admin.php?page=duplicator-tools&tab=diagnostics&section=info' ); ?>" method="post">
 	<?php wp_nonce_field( 'duplicator_settings_page', '_wpnonce', false ); ?>
 	<input type="hidden" id="dup-settings-form-action" name="action" value="">
 	<br/>
@@ -6,6 +6,17 @@
 	<?php if (! empty($action_response))  :	?>
 		<div id="message" class="notice notice-success is-dismissible"><p><?php echo $action_response; ?></p></div>
 	<?php endif; ?>
+
+<style>
+	<?php echo isset($css_hide_msg) ? $css_hide_msg : ''; ?>
+	div.success {color:#4A8254}
+	div.failed {color:red}
+	table.dup-reset-opts td:first-child {font-weight: bold}
+	table.dup-reset-opts td {padding:10px}
+	form#dup-settings-form {padding: 0px 10px 0px 10px}
+	button.dup-fixed-btn {min-width: 150px; text-align: center}
+	div#dup-tools-delete-moreinfo {display: none; padding: 5px 0 0 20px; border:1px solid silver; background-color: #fff; border-radius: 5px; padding:10px; margin:5px; width:750px }
+</style>
 
 	<?php
 		include_once 'inc.data.php';
