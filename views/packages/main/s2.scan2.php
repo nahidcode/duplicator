e1a010cf434caf62917448311f695100cbd2026b
Robert Riley <bob@snapcreek.com>
2018-10-25 06:29:25 -0700

    added closing parenthesis

--- views/packages/main/s2.scan2.php
+++ views/packages/main/s2.scan2.php
@@ -157,7 +157,7 @@ WP SETTINGS -->
 		//CACHE DIR
 		$cache_path = $cache_path = DUP_Util::safePath(WP_CONTENT_DIR) . '/cache';
 		$cache_size = DUP_Util::byteSize(DUP_Util::getDirectorySize($cache_path));
-		echo '<hr size="1" /><span id="data-srv-wp-cache"></span>&nbsp;<b>' . __('Cache Path', 'duplicator') . ":</b>&nbsp; '".esc_html($cache_path)."' (".esc_html($cache_size)." <br/>";
+		echo '<hr size="1" /><span id="data-srv-wp-cache"></span>&nbsp;<b>' . __('Cache Path', 'duplicator') . ":</b>&nbsp; '".esc_html($cache_path)."' (".esc_html($cache_size).") <br/>";
 		esc_html_e("Cached data will lead to issues at install time and increases your archive size. Empty your cache directory before building the package by using  "
 			. "your cache plugins clear cache feature.  Use caution if manually removing files the cache folder. The cache "
 			. "size minimum threshold that triggers this warning is currently set at ", 'duplicator');
