78f8580c6494dea3187c685ed04e32cc8821850d
Prashant Baldha <pmbaldha@gmail.com>
2018-11-13 21:39:50 +0530

    Final resolve: `Confirm form resubmission` error

--- installer/dup-installer/main.installer.php
+++ installer/dup-installer/main.installer.php
@@ -29,14 +29,9 @@
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-@session_cache_limiter("nocache");
-// @session_cache_limiter('must-revalidate');
+@header("Cache-Control: no cache");
+@session_cache_limiter("private_no_expire");
 @session_start();
-
-@header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-@header("Cache-Control: post-check=0, pre-check=0", false);
-@header("Pragma: no-cache");
-header("Expires: 0");
 
 date_default_timezone_set('UTC'); // Some machines don’t have this set so just do it here.
 
--- installer/installer.tpl
+++ installer/installer.tpl
@@ -20,15 +20,9 @@
   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-@session_cache_limiter("nocache");
-// @session_cache_limiter('must-revalidate');
+@header("Cache-Control: no cache");
+@session_cache_limiter("private_no_expire");
 @session_start();
-
-@header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-@header("Cache-Control: post-check=0, pre-check=0", false);
-@header("Pragma: no-cache");
-@header("Expires: 0");
-
 
 /******************************************************************
  * 
