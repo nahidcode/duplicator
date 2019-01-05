caf76b0a026d21a365ab08a2bb10bf2b72ff04b1
Andrea Leoni <a.leoni@amkdeveloper.com>
2019-01-05 14:53:46 +0100

    Reset active packages function ok

--- ctrls/class.web.services.php
+++ ctrls/class.web.services.php
@@ -15,12 +15,13 @@ class DUP_Web_Services
     /**
      * reset all ajax action
      *
+     * the output must be json
      */
     public static function ajax_reset_all()
     {
         ob_start();
         try {
-            /** Execute function **/
+            /** Execute function * */
             $error  = false;
             $result = array(
                 'data' => array(),
@@ -28,7 +29,20 @@ class DUP_Web_Services
                 'message' => ''
             );
 
-            $result['message'] = 'ok';
+            $noCompletePakcs = DUP_Package::get_all_by_status(array(
+                    array('op' => '<', 'status' => DUP_PackageStatus::COMPLETE)
+            ));
+
+            /** Delete all not completed packages * */
+            foreach ($noCompletePakcs as $pack) {
+                $pack->delete();
+            }
+
+            /** reset active package id * */
+            DUP_Settings::Set('active_package_id', -1);
+
+            /** Clean tmp folder * */
+            DUP_Package::not_active_files_tmp_cleanup();
 
             //throw new Exception('force error test');
         } catch (Exception $e) {
@@ -36,10 +50,10 @@ class DUP_Web_Services
             $result['message'] = $e->getMessage();
         }
 
-        /** Intercept output **/
+        /** Intercept output * */
         $result['html'] = ob_get_clean();
 
-        /** check error and return json **/
+        /** check error and return json * */
         if ($error) {
             wp_send_json_error($result);
         } else {
