fa8e8881522368a9a3098f325ef13def4d19d75c
Prashant Baldha <pmbaldha@gmail.com>
2019-02-27 09:08:56 +0530

    WIP: LT: Installer step 2 JS errors Part 2

--- installer/dup-installer/ctrls/ctrl.s2.base.php
+++ installer/dup-installer/ctrls/ctrl.s2.base.php
@@ -153,19 +153,24 @@ if ($_POST['dbaction'] == 'manual') {
     print_r(json_encode($dbinstall->writeInChunks()));
     die();
 } */ elseif(isset($_POST['continue_chunking']) && ($_POST['continue_chunking'] === 'false' && $_POST['pass'] == 1)) {
-    if ($dbinstall->verifyDBInstall()) {
+    $rowCountMisMatchTables = $dbinstall->getRowCountMisMatchTables();
+    if (empty($rowCountMisMatchTables)) {
 		$JSON['pass'] = 1;
 	} else {
 		$JSON['error'] = 1;
-		$JSON['error_message'] = 'ERROR: Table row count varification was failed';
+        $JSON['error_message'] = 'Database Table row count verification was failed for table(s):'
+                                    .implode(', ', $rowCountMisMatchTables).'. '
+                                    .'For additional information please visit .';
 	}
 } elseif(!isset($_POST['continue_chunking'])) {
-	$dbinstall->writeInDB();
-    if ($dbinstall->verifyDBInstall()) {
+	$rowCountMisMatchTables = $dbinstall->getRowCountMisMatchTables();
+    if (empty($rowCountMisMatchTables)) {
 		$JSON['pass'] = 1;
 	} else {
 		$JSON['error'] = 1;
-		$JSON['error_message'] = 'ERROR: Table row count varification was failed';
+		$JSON['error_message'] = 'Database Table row count verification was failed for table(s):'
+                                    .implode(', ', $rowCountMisMatchTables).'. '
+                                    .'For additional information please visit .';
 	}
 }
 
