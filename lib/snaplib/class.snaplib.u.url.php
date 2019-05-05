87aa6d962e961e955121a6df9766d7252e941114
andrea <andrea@andrea-N8xEJEK>
2019-05-05 13:08:02 +0200

    Snap lib updated

--- lib/snaplib/class.snaplib.u.url.php
+++ lib/snaplib/class.snaplib.u.url.php
@@ -12,48 +12,51 @@
  */
 defined('ABSPATH') || defined('DUPXABSPATH') || exit;
 
-class SnapLibURLU
-{
+if (!class_exists('SnapLibURLU', false)) {
 
-    /**
-     * Append a new query value to the end of a URL
-     *
-     * @param string $url   The URL to append the new value to
-     * @param string $key   The new key name
-     * @param string $value The new key name value
-     *
-     * @return string Returns the new URL with with the query string name and value
-     */
-    public static function appendQueryValue($url, $key, $value)
+    class SnapLibURLU
     {
-        $separator    = (parse_url($url, PHP_URL_QUERY) == NULL) ? '?' : '&';
-        $modified_url = $url."$separator$key=$value";
 
-        return $modified_url;
-    }
+        /**
+         * Append a new query value to the end of a URL
+         *
+         * @param string $url   The URL to append the new value to
+         * @param string $key   The new key name
+         * @param string $value The new key name value
+         *
+         * @return string Returns the new URL with with the query string name and value
+         */
+        public static function appendQueryValue($url, $key, $value)
+        {
+            $separator    = (parse_url($url, PHP_URL_QUERY) == NULL) ? '?' : '&';
+            $modified_url = $url."$separator$key=$value";
 
-    /**
-     * Fetches current URL via php
-     *
-     * @param bool $queryString If true the query string will also be returned.
-     *
-     * @returns The current page url
-     */
-    public static function getCurrentUrl($queryString = true)
-    {
-        $protocol = 'http';
-        if ($_SERVER['SERVER_PORT'] == 443 || (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')) {
-            $protocol     .= 's';
-            $protocolPort = $_SERVER['SERVER_PORT'];
-        } else {
-            $protocolPort = 80;
+            return $modified_url;
         }
-        $host    = $_SERVER['HTTP_HOST'];
-        $port    = $_SERVER['SERVER_PORT'];
-        $request = $_SERVER['PHP_SELF'];
 
-        $query = ($queryString === TRUE) ? $_SERVER['QUERY_STRING'] : "";
-        $url   = $protocol.'://'.$host.($port == $protocolPort ? '' : ':'.$port).$request.(empty($query) ? '' : '?'.$query);
-        return $url;
+        /**
+         * Fetches current URL via php
+         *
+         * @param bool $queryString If true the query string will also be returned.
+         *
+         * @returns The current page url
+         */
+        public static function getCurrentUrl($queryString = true)
+        {
+            $protocol = 'http';
+            if ($_SERVER['SERVER_PORT'] == 443 || (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')) {
+                $protocol     .= 's';
+                $protocolPort = $_SERVER['SERVER_PORT'];
+            } else {
+                $protocolPort = 80;
+            }
+            $host    = $_SERVER['HTTP_HOST'];
+            $port    = $_SERVER['SERVER_PORT'];
+            $request = $_SERVER['PHP_SELF'];
+
+            $query = ($queryString === TRUE) ? $_SERVER['QUERY_STRING'] : "";
+            $url   = $protocol.'://'.$host.($port == $protocolPort ? '' : ':'.$port).$request.(empty($query) ? '' : '?'.$query);
+            return $url;
+        }
     }
 }
