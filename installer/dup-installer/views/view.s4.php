349fa9dd307e92effb47af9eac57abcb202a0954
Bob Riley <bob@snapcreek.com>
2019-05-07 06:40:21 -0700

    fixed faq link

--- installer/dup-installer/views/view.s4.php
+++ installer/dup-installer/views/view.s4.php
@@ -124,7 +124,7 @@ However if you see a large amount of errors or you experience an issue with your
 <ul>
     <li>
         <b>Unknown collation:</b> See Online FAQ:
-        <a href="https://snapcreek.com/duplicator/docs/faqs-tech/#faq-trouble-090-q" target="_blank">What is Compatibility mode & 'Unknown collation' errors?</a>
+        <a href="https://snapcreek.com/duplicator/docs/faqs-tech/#faq-installer-110-q" target="_blank">What is Compatibility mode & 'Unknown collation' errors?</a>
     </li>
     <li>
         <b>Query Limits:</b> Update MySQL server with the <a href="https://dev.mysql.com/doc/refman/5.5/en/packet-too-large.html" target="_blank">max_allowed_packet</a>
