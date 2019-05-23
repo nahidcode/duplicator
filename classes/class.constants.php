dfa29093ad97571f883db4843aa8032a1b669b14
Prashant Baldha <pmbaldha@gmail.com>
2019-05-23 21:00:09 +0530

    LT Ticket: Updated to 1.3.12 = Host Build Interrup part 2

--- classes/class.constants.php
+++ classes/class.constants.php
@@ -2,5 +2,5 @@
 defined("ABSPATH") || exit;
 class DUP_Constants
 {
-    const ZIP_STRING_LIMIT = 500000;   // Cutoff for using ZipArchive addtostring vs addfile
+    const ZIP_STRING_LIMIT = 70000;   // Cutoff for using ZipArchive addtostring vs addfile
 }
