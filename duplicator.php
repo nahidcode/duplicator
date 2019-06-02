6420ca1bc71c9c2ede8ce088a7cb959c45c0d5a7
Prashant Baldha <pmbaldha@gmail.com>
2019-06-02 16:24:55 +0530

    LT: Auto select duparchive when installed on GoDaddy

--- duplicator.php
+++ duplicator.php
@@ -162,6 +162,15 @@ if (is_admin() == true)
     if ($isWPEngineHost) {
         require_once 'classes/host/class.wpengine.host.php';
     }
+
+    $hostName = gethostname();
+    $goDaddyHostNameSuffix = '.secureserver.net';
+    $lenGoDaddyHostNameSuffix = strlen($goDaddyHostNameSuffix);
+    $isGoDaddyHost = apply_filters('duplicator_godaddy_host_check', (false !== $hostName && substr($hostName, - $lenGoDaddyHostNameSuffix) === $goDaddyHostNameSuffix));
+    if ($isGoDaddyHost) {
+        require_once 'classes/host/class.godaddy.host.php';        
+    }
+
     require_once 'classes/class.settings.php';
     require_once 'classes/class.logging.php';    
     require_once 'classes/utilities/class.u.php';
