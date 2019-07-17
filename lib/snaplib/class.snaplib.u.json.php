fc98f342a4a790c16e8ed2e3f882a84aa39e2deb
andrea <andrea@andrea-N8xEJEK>
2019-07-17 11:21:44 +0200

    json callback fix for php 5.2

--- lib/snaplib/class.snaplib.u.json.php
+++ lib/snaplib/class.snaplib.u.json.php
@@ -136,7 +136,7 @@ if (!class_exists('DupLiteSnapJsonU', false)) {
 
                 case 'array':
                     // Arrays must be mapped in case they also return objects.
-                    return array_map('self::_wp_json_prepare_data', $data);
+                    return array_map(array(__CLASS__, '_wp_json_prepare_data'), $data);
 
                 case 'object':
                     // If this is an incomplete object (__PHP_Incomplete_Class), bail.
