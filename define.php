ce837005e2608e76ee101d3615b2fbb123e158a9
lifeinthegrid <cory@lifeinthegrid.com>
2020-03-10 18:34:58 -0700

    - Version update

--- define.php
+++ define.php
@@ -5,7 +5,7 @@ defined('ABSPATH') || defined('DUPXABSPATH') || exit;
 if (function_exists('plugin_dir_url')) 
 {		
     define('DUPLICATOR_VERSION',        '1.3.29');
-	define('DUPLICATOR_VERSION_BUILD',  '2020-03-07_10:30');
+	define('DUPLICATOR_VERSION_BUILD',  '2020-03-10_10:30');
     define('DUPLICATOR_PLUGIN_URL',     plugin_dir_url(__FILE__));
 	define('DUPLICATOR_SITE_URL',		get_site_url());
 	
